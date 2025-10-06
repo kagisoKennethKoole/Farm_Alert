3a495ae5df430ad2b3de3c73e668d553d8eaa350
Kagiso Kenneth Koole <Kagisokennethkoole@gmail.com>
2025-10-06 20:27:33 +0200

    add homepage and added redirect to home code on index page

---
+++ Pages/home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Farm_Alert.Pages
+{
+    public class homeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
