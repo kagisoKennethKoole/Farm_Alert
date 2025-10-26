164f20dc4d92943a3f708f5f33e8c0e319c93493
Kagiso Kenneth Koole <Kagisokennethkoole@gmail.com>
2025-10-26 08:08:01 +0200

    added dashborad and resource page

---
+++ Pages/Resources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Farm_Alert.Pages
+{
+    public class ResourcesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
